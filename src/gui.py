f4d16ece6adb2e48088dab03db06da29486be366
Mario <mario@supermario.dk>
2026-08-27 15:31:52 +0200

    Enhance GUI layout by adding title and adjusting button alignment in Pant Bottle Recognition

--- src/gui.py
+++ src/gui.py
@@ -1,7 +1,8 @@
 import sys
 
 from PyQt6.QtWidgets import QApplication
-from PyQt6.QtWidgets import QApplication, QPushButton, QVBoxLayout, QWidget
+from PyQt6.QtWidgets import QApplication, QPushButton, QVBoxLayout, QWidget, QLabel
+from PyQt6.QtCore import Qt
 
 
 def run_gui():
@@ -11,18 +12,31 @@ def run_gui():
     window.setWindowTitle("Pant Bottle Recognition")
     window.resize(800, 600)
     layout = QVBoxLayout()
-    width, height = 200, 60
+    width, height = 400, 60
 
     bottle_image_button = QPushButton("Import image of a bottle")
     bottle_camera_button = QPushButton(
         "Use the camera to determine if a bottle has pant"
     )
+    title = QLabel("Pant Bottle Recognition")
+    title.setAlignment(Qt.AlignmentFlag.AlignCenter)
+    title.setStyleSheet("""
+        font-size: 28px;
+        font-weight: bold;
+    """)
 
     bottle_image_button.setFixedSize(width, height)
     bottle_camera_button.setFixedSize(width, height)
+    layout.addWidget(title)
+    layout.addStretch()
+    layout.addWidget(
+        bottle_image_button,
+        alignment=Qt.AlignmentFlag.AlignHCenter,
+    )
+    layout.addWidget(
+        bottle_camera_button,
+        alignment=Qt.AlignmentFlag.AlignHCenter)
 
-    layout.addWidget(bottle_image_button)
-    layout.addWidget(bottle_camera_button)
 
     window.setLayout(layout)
 
