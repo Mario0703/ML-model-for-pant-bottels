2bb383459a974f05946023cf1adf127605a63de2
Mario <mario@supermario.dk>
2026-08-27 15:28:19 +0200

    Add GUI components for image import and camera usage in Pant Bottle Recognition

--- src/gui.py
+++ src/gui.py
@@ -1,6 +1,7 @@
 import sys
 
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QApplication
+from PyQt6.QtWidgets import QApplication, QPushButton, QVBoxLayout, QWidget
 
 
 def run_gui():
@@ -9,6 +10,22 @@ def run_gui():
     window = QWidget()
     window.setWindowTitle("Pant Bottle Recognition")
     window.resize(800, 600)
+    layout = QVBoxLayout()
+    width, height = 200, 60
+
+    bottle_image_button = QPushButton("Import image of a bottle")
+    bottle_camera_button = QPushButton(
+        "Use the camera to determine if a bottle has pant"
+    )
+
+    bottle_image_button.setFixedSize(width, height)
+    bottle_camera_button.setFixedSize(width, height)
+
+    layout.addWidget(bottle_image_button)
+    layout.addWidget(bottle_camera_button)
+
+    window.setLayout(layout)
+
     window.show()
 
     return app.exec()
